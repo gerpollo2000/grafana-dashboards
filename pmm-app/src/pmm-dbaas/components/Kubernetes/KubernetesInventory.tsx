54f986521ff3d5034bbd6768145857295711b813
Roman Misyurin <roman.misyurin@percona.com>
2020-08-28 16:48:39 +0300

    PMM-6492 add modal to kubernetes clusters

--- pmm-app/src/pmm-dbaas/components/Kubernetes/KubernetesInventory.tsx
+++ pmm-app/src/pmm-dbaas/components/Kubernetes/KubernetesInventory.tsx
@@ -1,12 +1,18 @@
 import React, { FC, useState } from 'react';
 import {
-  Button, HorizontalGroup, Modal, useTheme
+  Button, HorizontalGroup, TextArea, useTheme
 } from '@grafana/ui';
 import { Table } from 'shared/components/Elements/Table/Table';
 import { Messages } from 'pmm-dbaas/DBaaS.messages';
 import { getStyles } from './Kubernetes.styles';
 import { useKubernetes } from './Kubernetes.hooks';
 import { Kubernetes } from './Kubernetes.types';
+import {Modal} from "../../../shared/components/Elements/Modal/Modal";
+import {styles} from "../../../pmm-inventory/Tabs/Tabs.styles";
+import {CheckboxField, FormElement} from "../../../shared/components/Form";
+import {Field, Form} from "react-final-form";
+import {InputFieldAdapter} from "../../../pmm-settings/components/PlatformLogin/FieldAdapters/FieldAdapters";
+import validators from "../../../shared/components/helpers/validators";
 
 export const KubernetesInventory: FC = () => {
   const theme = useTheme();
@@ -42,10 +48,20 @@ export const KubernetesInventory: FC = () => {
 
   return (
     <div className={styles.tableWrapper}>
+      <div className={styles.actionPanel}>
+        <Button
+          size="md"
+          onClick={() => setModalVisible(!modalVisible)}
+          icon="trash-alt"
+          variant="destructive"
+        >
+          {'add cluster'}
+        </Button>
+      </div>
       <Modal
         title={Messages.kubernetes.deleteModal.title}
-        isOpen={modalVisible}
-        onDismiss={() => setModalVisible(false)}
+        isVisible={modalVisible}
+        onClose={() => setModalVisible(false)}
       >
         <h4 className={styles.deleteModalContent}>
           {Messages.kubernetes.deleteModal.confirmMessage}
@@ -72,6 +88,31 @@ export const KubernetesInventory: FC = () => {
           </Button>
         </HorizontalGroup>
       </Modal>
+      {/*<Modal*/}
+      {/*  title={Messages.kubernetes.deleteModal.title}*/}
+      {/*  isOpen={modalVisible}*/}
+      {/*  onDismiss={() => setModalVisible(false)}*/}
+      {/*>*/}
+      {/*  <h4 className={styles.deleteModalContent}>*/}
+      {/*    {Messages.kubernetes.deleteModal.getConfirmMessage(selected.length)}*/}
+      {/*  </h4>*/}
+      {/*  <HorizontalGroup justify="space-between" spacing="md">*/}
+      {/*    <Button variant="secondary" size="md" onClick={() => setModalVisible(false)} data-qa="cancel-delete-kubernetes-button">*/}
+      {/*      {Messages.kubernetes.deleteModal.cancel}*/}
+      {/*    </Button>*/}
+      {/*    <Button*/}
+      {/*      variant="destructive"*/}
+      {/*      size="md"*/}
+      {/*      onClick={() => {*/}
+      {/*        deleteKubernetes(selected.map((row) => row.original));*/}
+      {/*        setModalVisible(false);*/}
+      {/*      }}*/}
+      {/*       data-qa="delete-kubernetes-button" */}
+      {/*    >*/}
+      {/*      {Messages.kubernetes.deleteModal.confirm}*/}
+      {/*    </Button>*/}
+      {/*  </HorizontalGroup>*/}
+      {/*</Modal>*/}
       <Table
         columns={columns}
         data={kubernetes}
